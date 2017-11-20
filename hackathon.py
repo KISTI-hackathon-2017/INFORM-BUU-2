75a281bdbaa80eeff86f3114e5cba1ae81e68130
BUU-Team <33752621+BUU-Team@users.noreply.github.com>
2017-11-20 22:44:12 +0700

    Add files via upload

--- hackathon.py
+++ hackathon.py
@@ -4,6 +4,7 @@ import pymysql
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import os.path
+
 # set this to True to force download database using SQL,
 # else {if `datafile` exists, load it. else download from database}
 download = False
@@ -38,6 +39,7 @@ m7 = m7[m7.timestamp < '2017-08']
 ##### remove outlier #########
 m7 = m7[m7.so2_value < 20]
 m7 = m7[m7.co_value < 10]
+#m7 = m7[m7.no2_value < 0.02]
 
 
 ############ Filter ##########
@@ -62,11 +64,35 @@ norm_no2 = ( m7.no2_value - m7.no2_value.min() )/( m7.no2_value.max() - m7.no2_v
 m7['norm_no2'] = norm_no2
 norm_co2 = ( m7.co_value - m7.co_value.min() )/( m7.co_value.max() - m7.co_value.min())
 m7['norm_co2'] = norm_co2
+norm_pm25 = ( pm25 - pm25.min() )/( pm25.max() - pm25.min())
+m7['norm_pm25'] = norm_pm25
+norm_pm10 = ( pm10 - pm10.min() )/( pm10.max() - pm10.min())
+m7['norm_pm10'] = norm_pm10
+norm_hum = ( hum - hum.min())/( hum.max() - hum.min())
+m7['norm_pm10'] = norm_hum
 
+
+
+###### group ######
+m7['timegroup'] = m7.timestamp.apply(lambda x: x.split()[0])
+grouped = m7.groupby('timegroup')
+so2mean = grouped.norm_so2.mean()
+no2mean = grouped.norm_no2.mean()
+co2mean = grouped.norm_co2.mean()
+pm25mean = grouped.norm_pm25.mean()
+pm10mean = grouped.norm_pm10.mean()
+
+
+##### norm ######
+
+so2mean = ( so2mean - so2mean.min() )/( so2mean.max() - so2mean.min())
+no2mean = ( no2mean - no2mean.min() )/( no2mean.max() - no2mean.min())
+co2mean = ( co2mean - co2mean.min() )/( co2mean.max() - co2mean.min())
+pm25mean = ( pm25mean - pm25mean.min() )/( pm25mean.max() - pm25mean.min())
+pm10mean = ( pm10mean - pm10mean.min() )/( pm10mean.max() - pm10mean.min())
 
 
 ###### sum in month #######
-
 Sumpm25 = pm25.cumsum()
 m7['Sumpm_25'] = Sumpm25
 Sumpm10 = pm10.cumsum()
@@ -74,9 +100,88 @@ m7['Sumpm_10'] = Sumpm10
 
 
 
+###### Plot Graph #######
+
+x = np.linspace(1, 31, num=31)
+
+
+#### pm25  ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.legend(['pm25'])
+
+#### pm10  ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.legend(['pm10'])
+
+
+#### so2  ####
+
+plt.plot(x, so2mean, '-', linewidth=2)
+plt.legend(['so2'])
+
+#### no2  ####
+
+plt.plot(x, no2mean, '-', linewidth=2)
+plt.legend(['no2'])
+
+#### co2  ####
+
+plt.plot(x, co2mean, '-', linewidth=2)
+plt.legend(['co2'])
+
+
+#### so2 and no2 ####
+
+plt.plot(x, so2mean, '-', linewidth=2)
+plt.plot(x, no2mean, '-', linewidth=2)
+plt.legend(['so2', 'no2'])
+
+#### so2 and co2 ####
+
+plt.plot(x, so2mean, '-', linewidth=2)
+plt.plot(x, co2mean, '-', linewidth=2)
+plt.legend(['so2', 'co2'])
+
+#### pm25 and pm10 ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.plot(x, pm10mean, '-', linewidth=2)
+plt.legend(['pm25', 'pm10'])
+
+#### pm25 , pm10 and so2 ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.plot(x, pm10mean, '-', linewidth=2)
+plt.plot(x, so2mean, '-', linewidth=2)
+plt.legend(['pm25', 'pm10','so2'])
+
+#### pm25 , pm10 and no2 ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.plot(x, pm10mean, '-', linewidth=2)
+plt.plot(x, no2mean, '-', linewidth=2)
+plt.legend(['pm25', 'pm10','no2'])
+
+#### pm25 , pm10 and co2 ####
+
+plt.plot(x, pm25mean, '-', linewidth=2)
+plt.plot(x, pm10mean, '-', linewidth=2)
+plt.plot(x, co2mean, '-', linewidth=2)
+plt.legend(['pm25', 'pm10','co2'])
+
+
+plt.show()
+
+
+
+#fig = plt.gcf()
+
+    
 ##### write file ######
 
-m7.to_csv('sensorData2.csv')
+#m7.to_csv('sensorData2.csv')
 
 
 
